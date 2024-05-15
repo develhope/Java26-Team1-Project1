b9038c781dd2509595968c41e49678e18a78b2e4
vince <vincenzo.costagliola@develhope.co>
2024-05-15 09:54:25 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(funzione());
+    }
 
+
+    private static String funzione() {
+        return "Hello world";
     }
 
 }
