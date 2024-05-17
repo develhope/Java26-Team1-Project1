a2255c6612335d90a1b38aaf8d07017de0cf8485
Riccardo <ric.cavaleri@gmail.com>
2024-05-17 11:09:54 +0200

    Divisione con resto

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(divisione(13,5));
+    } public static String divisione (int a ,int b){
+    String result;
 
+    result = "risultato: " + a / b + " resto: " + a % b;
+        return result;
     }
 
 }
