4b39e5df5850c0663d4f2297c2d1b50574e10e08
Riccardo <ric.cavaleri@gmail.com>
2024-05-17 11:16:50 +0200

    Divisione con resto

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(divisione(13,5));
+
     } public static String divisione (int a ,int b){
     String result;
 
