ea678fa6a92cab9f0a88a0301bccc8934fbd1853
iresha warnakulasuriya <dilhanimadu2002@gmail.com>
2024-05-17 12:17:51 +0200

    metodo addizione fixed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(addition(10,2));
     }
     // method to calculate the addition of two integers
     public static String addition(int a, int b){
