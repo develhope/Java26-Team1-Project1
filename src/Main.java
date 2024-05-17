e22831275f03391296bb3c9098dc929ef05eb6b7
Riccardo <ric.cavaleri@gmail.com>
2024-05-17 11:31:30 +0200

    Moltiplicazione

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println(moltiplicazione(15,10));
+
     } public static String moltiplicazione (int a ,int b){
     String result;
 
