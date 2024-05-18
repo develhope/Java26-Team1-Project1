6cbdc1646de6019b138440b9d4c2f4ebaea45d1c
Michele Angeletti <m.angeletti99@gmail.com>
2024-05-18 11:03:23 +0200

    added: some methods invoked

--- src/Main.java
+++ src/Main.java
@@ -11,43 +11,41 @@ public class Main {
         // Leggo e salvo il primo numero
         int num1 = 0;
         int readingPoint = 0;
-        for(int i = 0; i< expressionChars.length; i++){
+        for (int i = 0; i < expressionChars.length; i++) {
             if (expressionChars[i] >= '0' && expressionChars[i] <= '9') {
-               num1 = num1 * 10 + (expressionChars[i] - 48); // 0 è 48 in ASCII e le altre cifre sono a seguire
-            }
-            else {
+                num1 = num1 * 10 + (expressionChars[i] - 48); // 0 è 48 in ASCII e le altre cifre sono a seguire
+            } else {
                 readingPoint = i;
                 break;
             }
         }
         // Leggo e salvo il secondo numero
         int num2 = 0;
-        for(int i = readingPoint + 1; i< expressionChars.length; i++){
+        for (int i = readingPoint + 1; i < expressionChars.length; i++) {
             if (expressionChars[i] >= '0' && expressionChars[i] <= '9') {
                 num2 = num2 * 10 + (expressionChars[i] - 48); // 0 è 48 in ASCII e le altre cifre sono a seguire
-            }
-            else {
+            } else {
                 break;
             }
         }
 
         // Controllo quale metodo richiamare per l'operazione
         String result = "prova";
-        switch(operation.toUpperCase()){
+        switch (operation.toUpperCase()) {
             case "SOMMA":
-                // Richiamo il metodo della somma
+                result = addition(num1, num2);
                 break;
             case "DIFFERENZA":
-                // Richiamo il metodo della differenza
+                result = sottrazione(num1, num2);
                 break;
             case "MOLTIPLICAZIONE":
-                // Richiamo il metodo della moltiplicazione
+                result = moltiplicazione(num1, num2);
                 break;
             case "DIVISIONE":
-                // Richiamo il metodo della divisione
+                result = divisione(num1, num2);
                 break;
             case "POTENZA":
-                // Richiamo il metodo della potenza
+                result = String.valueOf(potenza(num1, num2));
                 break;
             case "PARI O DISPARI":
                 // Richiamo il metodo per vedere se è pari o dispari
@@ -58,6 +56,7 @@ public class Main {
         }
 
         // Stampo il risultato
+        System.out.println(expression);
         System.out.println(result);
     }
 
