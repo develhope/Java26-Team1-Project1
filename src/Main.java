bb5d12d2c50948dcda052cc5ab02331de6aac99a
Santo La Rocca <laroccasantoandrea@gmail.com>
2024-05-19 19:55:38 +0200

    creatata funzione per calcolare la potenza

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static int potenza(int num, int pot){
 
         int res = num;
+
         for(int i=1; i<pot; i++){
             res = res * num;
+        }
+        if(pot == 0){
+            res = 1;
         }
         return res;
     }
